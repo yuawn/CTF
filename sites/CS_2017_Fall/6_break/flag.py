c51abd702ca5b580b113940a25ef55bf418460ab
yuan <ssspeed00@gmail.com>
2017-12-19 14:35:47 +0800

    CTF

---
+++ sites/CS_2017_Fall/6_break/flag.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import angr
+
+# CTF{PinADXAnInterfaceforCustomizableDebuggingwithDynamicInstrumentation}
+
+p = angr.Project( './break' )
+
+state = p.factory.entry_state()
+#state = p.factory.full_init_state( args = ['./break'] )
+
+g = range( 32 )
+
+s = 'CTF{PinADXAnInterfaceforCustomizableDebuggingwithDynamicIns'
+
+for c in s:
+    k = state.posix.files[0].read_from(1)
+    state.solver.add( k == c )
+
+k = state.posix.files[0].read_from(1)
+state.solver.add( k != 10 )
+
+state.posix.files[0].seek(0)
+state.posix.files[0].length = 89
+
+sm = p.factory.simulation_manager( state )
+
+gg = [ 0x400831 , 0x4008ff ]
+
+sm.explore( find = 0x40091f , avoid = gg  )
+
+print sm.found[0].posix.dumps(0)
--- sites/CS_2017_Fall/prac_calc/README.md
+++ sites/CS_2017_Fall/prac_calc/README.md
@@ -1,5 +1,6 @@
 # Prac - calc
 ## Windows reverse
 * Windows API break points.
+* `GetWindowsTextW`.
 * Find password.
 * flag{31337_is_so_l33t}
